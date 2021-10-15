3d8236380cb09188601c416b402d69af66a7c625
DSmithMR <dsmith.mr@gmail.com>
2021-10-14 19:33:04 -0500

    Paginas iniciales

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import SignInSide from './Componentes/Login/SignInSide';
+import SignUp from './Componentes/Login/SignUp';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={SignInSide} />
+          <Route exact path="/SignUp" component={SignUp} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/Componentes/Home/Home.js
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+
+export default function Types() {
+  return (
+    <Box sx={{ width: '100%', maxWidth: 500 }}>
+      
+    </Box>
+  );
+}
